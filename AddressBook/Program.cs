1f63a5bb12187f4adbf59a7a3dbc01c09af0508e
Anant1202 <anantsingh1202@gmail.com>
2022-06-12 18:52:21 +0530

    Count

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,42 +4,135 @@ namespace AddressBook
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static Dictionary<string, List<AddDetails>> addressBook = new Dictionary<string, List<AddDetails>>();
+        public static Dictionary<string, List<AddDetails>> City = new Dictionary<string, List<AddDetails>>();
+        public static Dictionary<string, List<AddDetails>> State = new Dictionary<string, List<AddDetails>>();
+        static void Main(string[] args)
         {
-            int choice;
-            Console.WriteLine("Welcome to Address Book Program");
-            do
+            Console.WriteLine("Welcome to Address Book System Program");
+            Console.WriteLine("Enter the number of address books: ");
+            int noOfAddressBook = Convert.ToInt32(Console.ReadLine());
+            int noOfBooks = 0;
+            while (noOfBooks < noOfAddressBook)
             {
-                Console.WriteLine("1.Add the details");
-                Console.WriteLine("2.Edit the details");
-                Console.WriteLine("3.Delete the details");
-                Console.WriteLine("4.Listing the Details");
-                Console.WriteLine("5.Exit");
-                Console.WriteLine("Enter your choice");
-                AddDetails obj = new AddDetails();
-                choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
+                Console.WriteLine("Enter the address book name : ");
+                string addressbookname = Console.ReadLine();
+                AddDetails addrBook = new AddDetails();
+                Console.WriteLine("Enter the no of contacts in the address book: ");
+                int noOfContact = Convert.ToInt32(Console.ReadLine());
+
+                while (noOfContact > 0)
+                {
+                    Console.WriteLine("Enter the details of contact to be added: ");
+
+                    Console.Write("Enter First Name: ");
+                    string FirstName = Console.ReadLine();
+
+                    Console.Write("Enter Last Name: ");
+                    string LastName = Console.ReadLine();
+
+                    Console.Write("Enter Phone Number: ");
+                    string PhoneNumber = Console.ReadLine();
+
+                    Console.Write("Enter Address : ");
+                    string Addresses = Console.ReadLine();
+
+                    Console.Write("Enter City : ");
+                    string City = Console.ReadLine();
+
+                    Console.Write("Enter State : ");
+                    string State = Console.ReadLine();
+
+                    Console.Write("Enter ZipCode: ");
+                    string ZipCode = Console.ReadLine();
+
+                    Console.Write("Enter EmailId: ");
+                    string EmailId = Console.ReadLine();
+                    addrBook.GetUserInfo(FirstName, LastName, PhoneNumber, Addresses, City, State, ZipCode, EmailId);
+                    noOfContact--;
+                    //addressBook.Add(addressbookname, addrBook.people);
+                    Console.WriteLine(" ");
+                    addrBook.ListingPeople();
+                }
+                Console.WriteLine("1.To modify the details");
+                Console.WriteLine("2.To remove the details");
+                Console.WriteLine("3.city or state");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
                 {
                     case 1:
-                        obj.GetUserInfo();
+                        addrBook.EditInfo();
+                        Console.WriteLine(" ");
+                        addrBook.ListingPeople();
                         break;
                     case 2:
-                        obj.EditInfo();
+                        addrBook.DeleteInfo();
+                        Console.WriteLine(" ");
+                        addrBook.ListingPeople();
                         break;
                     case 3:
-                        obj.DeleteInfo();
-                        break;
-                    case 4:
-                        obj.ListingPeople();
-                        break;
-                    case 5:
-                        return;
-                    default:
-                        Console.WriteLine("Enter a valid option");
+                        Console.WriteLine("Enter 1-To Search a person through a City");
+                        Console.WriteLine("Enter 2-To Search a person through a State");
+                        Console.WriteLine("Enter 3-To view a person by state list or city list");
+                        int opt = Convert.ToInt32(Console.ReadLine());
+                        switch (opt)
+                        {
+
+                            case 1:
+                                SearchAddress(opt);
+                                break;
+                            case 2:
+                                SearchAddress(opt);
+                                break;
+                            case 3:
+                                AddDetails.DisplayCityorState();
+
+                                break;
+                            default:
+                                Console.WriteLine("Invalid Option!");
+                                break;
+                        }
                         break;
                 }
+                if (addressBook.ContainsKey(addressbookname))
+                {
+                    Console.WriteLine("Existing address book name");
+                    return;
+                }
+                else
+                {
+                    addressBook.Add(addressbookname, addrBook.people);
+                }
+                noOfBooks++;
+                foreach (KeyValuePair<string, List<AddDetails>> addr in addressBook)
+                {
+                    Console.WriteLine("The address Books are:{0}", addr.Key);
 
-            } while (choice != 5);
+                }
+            }
+            static void SearchAddress(int option)
+            {
+                string city, state;
+                if (option == 1)
+                {
+                    Console.WriteLine("Enter the City Name");
+                    city = Console.ReadLine();
+                    foreach (KeyValuePair<string, List<AddDetails>> kvp in addressBook)
+                    {
+                        AddDetails.StoreCityList(kvp.Key, kvp.Value, city);
+
+                    }
+                    if (option == 2)
+                    {
+                        Console.WriteLine("Enter the City Name");
+                        state = Console.ReadLine();
+                        foreach (KeyValuePair<string, List<AddDetails>> kvp in addressBook)
+                        {
+                            AddDetails.StoreStateList(kvp.Key, kvp.Value, state);
+                        }
+                    }
+                }
+            }
         }
     }
 }
